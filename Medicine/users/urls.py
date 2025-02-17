89a4de611b7637b83eff9fdfd0ed4efa06fb5940
Enris <ernis.osmonkulov.05@gmail.com>
2025-02-17 23:47:26 +0600

    add JWT

--- Medicine/users/urls.py
+++ Medicine/users/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     path('doctor_list/', DoctorListView.as_view(), name='doctor-list'),
     path('doctor_detail/<int:pk>/', DoctorDetailListView.as_view(), name='doctor-detail'),
 
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+
     path('feedback_create', FeedbackCreateViewAPI.as_view(), name='feedback_create'),
     path('feedback_list', FeedbackListViewAPI.as_view(), name='feedback_create'),
 
